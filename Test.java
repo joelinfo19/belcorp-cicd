7d7fbfb93f52de9b799a132f9b0da4fae2afbdbf
joelinfo19 <joel_zerg_pro@outlook.com>
2024-09-12 17:05:17 -0500

    udpate_taks

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 // test.java
 public class Test {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo desde Test!");
+	System.out.println("Hola Mundo");
+        try {
+            Thread.sleep(100000); // Mantiene el contenedor en ejecución
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
